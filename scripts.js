62299d6f7d692bb2b50a013da5d03d0a54ec72b8
devmata <mfacuna@ing.ucsc.cl>
2024-01-30 16:18:00 -0300

    fijar menu

--- scripts.js
+++ scripts.js
@@ -1,32 +1,47 @@
 // Función para simular el efecto de escritura
 function escribirTexto(elemento, texto, velocidad) {
-    let i = 0;
-    const intervaloH1 = setInterval(function () {
-        elemento.innerHTML += texto.charAt(i);
-        i++;
-        if (i >= texto.length) {
-            clearInterval(intervaloH1);
-            // Una vez que ha terminado de agregar texto al h1, agregar texto al span
-            agregarTextoAlSpan();
-        }
-    }, velocidad);
-
-    function agregarTextoAlSpan() {
-        const span = document.createElement("span");
-        span.id = "span-titulo"; // Asignar un id al nuevo span
-        elemento.appendChild(span);
-
-        let j = 0;
-        let textoSpan = 'Dev'
-        const intervaloSpan = setInterval(function () {
-            span.innerHTML += textoSpan.charAt(j);
-            j++;
-            if (j >= textoSpan.length) {
-                clearInterval(intervaloSpan);
-            }
-        }, velocidad);
+  let i = 0;
+  const intervaloH1 = setInterval(function () {
+    elemento.innerHTML += texto.charAt(i);
+    i++;
+    if (i >= texto.length) {
+      clearInterval(intervaloH1);
+      // Una vez que ha terminado de agregar texto al h1, agregar texto al span
+      agregarTextoAlSpan();
     }
+  }, velocidad);
+
+  function agregarTextoAlSpan() {
+    const span = document.createElement("span");
+    span.id = "span-titulo"; // Asignar un id al nuevo span
+    elemento.appendChild(span);
+
+    let j = 0;
+    let textoSpan = "Dev";
+    const intervaloSpan = setInterval(function () {
+      span.innerHTML += textoSpan.charAt(j);
+      j++;
+      if (j >= textoSpan.length) {
+        clearInterval(intervaloSpan);
+      }
+    }, velocidad);
+  }
 }
 
 const titulo = document.getElementById("titulo-header");
 escribirTexto(titulo, "atias Acuña ", 80);
+
+// Selecciona el elemento que deseas modificar después de hacer scroll
+const menu = document.querySelector('.menu');
+
+// Función que se ejecutará cuando se haga scroll
+window.addEventListener('scroll', function() {
+  // Verifica la posición de desplazamiento (puedes ajustar el valor según tus necesidades)
+  if (window.scrollY > 50) {
+    // Agrega una clase cuando el scroll alcanza cierta posición
+    menu.classList.add('scrolled');
+  } else {
+    // Elimina la clase cuando el scroll vuelve a la posición inicial
+    menu.classList.remove('scrolled');
+  }
+});
